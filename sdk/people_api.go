1a78c2d57797b739c6692c7c6fb43414c2e1ae9d
Jose Bogarin Solano <jbogarin@altus.co.cr>
2020-03-24 19:38:07 -0600

    Removed commented Timezone

--- sdk/people_api.go
+++ sdk/people_api.go
@@ -45,26 +45,26 @@ type PersonRequest struct {
 
 // Person is the Person definition
 type Person struct {
-	ID           string              `json:"id,omitempty"`           // Person ID.
-	Emails       []string            `json:"emails,omitempty"`       // Person email array.
-	SIPAddresses []PersonSIPAddress  `json:"sipAddresses,omitempty"` // Person SIP Addresses
-	PhoneNumbers []PersonPhoneNumber `json:"phoneNumbers,omitempty"` //Person phone numbers
-	DisplayName  string              `json:"displayName,omitempty"`  // Person display name.
-	NickName     string              `json:"nickName,omitempty"`     // Person nickname.
-	FirstName    string              `json:"firstName,omitempty"`    // Person first name.
-	LastName     string              `json:"lastName,omitempty"`     // Person last name.
-	Avatar       string              `json:"avatar,omitempty"`       // Person avatar URL.
-	OrgID        string              `json:"orgId,omitempty"`        // Person organization ID.
-	Roles        []string            `json:"roles,omitempty"`        // Person roles.
-	Licenses     []string            `json:"licenses,omitempty"`     // Person licenses.
-	Created      time.Time           `json:"created,omitempty"`      // Person creation date/time.
-	LastModified time.Time           `json:"lastModified,omitempty"` // Person last modified
-	// TimeZone      string              `json:"timeZone,omitempty"`      // Person time zone.
-	LastActivity  time.Time `json:"lastActivity,omitempty"`  // Person last active date/time.
-	Status        string    `json:"status,omitempty"`        // Person presence status
-	InvitePending bool      `json:"invitePending,omitempty"` // Person invite pending
-	LoginEnabled  bool      `json:"loginEnabled,omitempty"`  // Person login Enabled
-	PersonType    string    `json:"type,omitempty"`          // Person type (person or bot).
+	ID            string              `json:"id,omitempty"`            // Person ID.
+	Emails        []string            `json:"emails,omitempty"`        // Person email array.
+	SIPAddresses  []PersonSIPAddress  `json:"sipAddresses,omitempty"`  // Person SIP Addresses
+	PhoneNumbers  []PersonPhoneNumber `json:"phoneNumbers,omitempty"`  //Person phone numbers
+	DisplayName   string              `json:"displayName,omitempty"`   // Person display name.
+	NickName      string              `json:"nickName,omitempty"`      // Person nickname.
+	FirstName     string              `json:"firstName,omitempty"`     // Person first name.
+	LastName      string              `json:"lastName,omitempty"`      // Person last name.
+	Avatar        string              `json:"avatar,omitempty"`        // Person avatar URL.
+	OrgID         string              `json:"orgId,omitempty"`         // Person organization ID.
+	Roles         []string            `json:"roles,omitempty"`         // Person roles.
+	Licenses      []string            `json:"licenses,omitempty"`      // Person licenses.
+	Created       time.Time           `json:"created,omitempty"`       // Person creation date/time.
+	LastModified  time.Time           `json:"lastModified,omitempty"`  // Person last modified
+	TimeZone      string              `json:"timeZone,omitempty"`      // Person time zone.
+	LastActivity  time.Time           `json:"lastActivity,omitempty"`  // Person last active date/time.
+	Status        string              `json:"status,omitempty"`        // Person presence status
+	InvitePending bool                `json:"invitePending,omitempty"` // Person invite pending
+	LoginEnabled  bool                `json:"loginEnabled,omitempty"`  // Person login Enabled
+	PersonType    string              `json:"type,omitempty"`          // Person type (person or bot).
 }
 
 // AddPerson is used to append a person to a slice of People
